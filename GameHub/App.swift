ff0cfb3fedb28ea97a5ffff74113dd3d0d02919e
sifatrabbioffice <sifatrabbioffice@gmail.com>
2026-05-05 20:08:55 +0600

    Create App.swift

---
+++ GameHub/App.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct GameHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("GameHub is Running!")
+                .font(.title)
+        }
+    }
+}
